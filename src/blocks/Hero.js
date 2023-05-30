7c620eedb3b58f0292bb86d462e8286b45e3c344
ahmadkokab <ahmadkokab@processton.com>
2023-05-31 02:19:54 +0500

    Changes to be committed: 	modified:   package.json 	modified:   src/blocks/Hero.js 	modified:   yarn.lock

--- src/blocks/Hero.js
+++ src/blocks/Hero.js
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import React from 'react'
+import { Parallax } from 'react-parallax';
 import Buttons from '../components/UI/Buttons'
 import Container from '../components/UI/Container'
 import Text from '../components/UI/Text'
@@ -10,9 +11,15 @@ export default function Hero({ data }) {
   const isFull = data?.variant === 'full'
   console.log(data);
   return (
+    <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={data?.bg_image?.imag}
+        bgImageAlt={data?.bg_image?.alt}
+        strength={-200}
+    >
     <section
       className={clsx(
-        'py-12 dark:bg-black dark:text-white lg:py-24',
+        'py-12 dark:bg-black dark:text-white lg:py-24 relative',
         {
           'flex min-h-screen items-center': isFull,
         },
@@ -21,15 +28,6 @@ export default function Hero({ data }) {
         },
       )}
     >
-      {data?.bg_image?.image && (
-        <div className="lg:w-1/2">
-            <Image
-              src={data?.bg_image?.image}
-              alt={data?.bg_image?.alt}
-              className="w-full"
-            />
-        </div>
-      )}
       <Container>
         {data?.title && (
           <h1
@@ -60,5 +58,6 @@ export default function Hero({ data }) {
         </div>
       )}
     </section>
+    </Parallax>
   )
 }
