454a07e2b6cf60b09a6a89d67b9a85b8cad1edb8
ahmadkokab <ahmadkokab@processton.com>
2023-06-08 16:33:56 +0500

    colors config test

--- cms/previews/Page.js
+++ cms/previews/Page.js
@@ -15,7 +15,7 @@ export default class PagePreview extends React.Component {
         {hasBlocks ? (
           <PageBuilder blocks={blocksUpdated} preview={true} />
         ) : (
-          <div class='py-24 text-center flex items-center justify-center'>
+          <div className='py-24 text-center flex items-center justify-center'>
             <h1>Add first block to start creating your website</h1>
           </div>
         )}
