5bf5d6a87822b4219ed12723cba56c1da90dbee6
Processton <68786248+processton@users.noreply.github.com>
2023-06-23 08:51:51 +0000

    commit

--- src/blocks/Hero.js
+++ src/blocks/Hero.js
@@ -42,7 +42,7 @@ export default function Hero({ data }) {
               },
             )}
           >
-            <div className='container mx-auto max-w-screen-xl flex'>
+            <div className='container mx-auto max-w-screen-xl flex flex-col-reverse md:flex-row gap-8 md:gap-4'>
               <Container className={`lg:w-2/3`}>
                 {data?.title && (
                   <h1
@@ -68,7 +68,7 @@ export default function Hero({ data }) {
                     <Image
                       src={data?.photo?.image}
                       alt={data?.photo?.alt}
-                      className="w-72 rounded-md"
+                      className="w-36 md:w-72 rounded-lg"
                     />
                 </div>
               ) : data?.hero_photo && data?.hero_photo?.hero_image && (
@@ -76,7 +76,7 @@ export default function Hero({ data }) {
                     <Image
                       src={data?.hero_photo?.hero_image}
                       alt={data?.hero_photo?.alt}
-                      className="w-72 rounded-md"
+                      className="w-36 md:w-72 rounded-lg"
                     />
                 </div>
               )}
--- src/components/Header.js
+++ src/components/Header.js
@@ -130,7 +130,7 @@ export default function Header() {
                         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                         <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </button>
-                    <DarkmodeToggle />
+                    <DarkmodeToggle className={"inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"}  />
                 </div>
                 <div className={clsx({
                         'hidden': !openDropDown,
@@ -140,7 +140,7 @@ export default function Header() {
                         nav.nav.map((button , i) => {
                           return <GatsbyLink className={
                             clsx({
-                              'block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-headerTextDefaultColor lg:hover:text-headerTextHoverColor dark:text-white': (button.type === 'default'),
+                              'block py-2 pr-4 pl-3 text-headerTextDefaultColor hover:text-headerTextHoverColor rounded lg:bg-transparent lg:text-headerTextDefaultColor lg:hover:text-headerTextHoverColor dark:text-white': (button.type === 'default'),
                               "text-headerTextDefaultColor hover:text-headerTextHoverColor bg-headerTextDefaultBG hover:bg-headerTextHoverBG focus:ring-4 focus:ring-headerTextHoverBG font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-headerTextDefaultBG dark:hover:bg-headerTextHoverBG focus:outline-none dark:focus:ring-headerTextHoverBG": button.type=== 'button'
                             })
                           }
@@ -152,7 +152,7 @@ export default function Header() {
                         })
                         
                       )}
-                    <DarkmodeToggle className="hidden lg:flex" />
+                    <DarkmodeToggle className="hidden md:block" />
                     </div>
                 </div>
             </div>
